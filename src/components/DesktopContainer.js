f5dbaf0233e1b0dba3ba92893a9196d49bc77b84
ian buchholz <ianbuchholz@gmail.com>
2018-10-25 05:12:55 -0700

    remove menu state and visibility from DesktopContainer, make single fixed navbar

--- src/components/DesktopContainer.js
+++ src/components/DesktopContainer.js
@@ -10,57 +10,41 @@ import {
   Menu,
   Responsive,
   Segment,
-  Visibility
 } from 'semantic-ui-react'
 
 class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
-
   render() {
     const { children } = this.props
-    const { fixed } = this.state
-
     return (
       <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
+        <Segment
+          inverted
+          textAlign='center'
+          style={{ minHeight: 700, padding: '1em 0em', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}
+          vertical
         >
-          <Segment
+          <Menu
+            style={{backgroundColor: 'rgba(0,0,0,.8)'}}
+            fixed={'top'}
+            size='large'
+            borderless
             inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}
-            vertical
           >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-              borderless
-            >
-              <Container>
-                <Menu.Item><Icon size='large' name='building outline'/></Menu.Item>
-                <Menu.Item as='a' active>Investing With Us</Menu.Item>
-                <Menu.Item as='a'>Properties</Menu.Item>
-                <Menu.Item as='a'>Investment Criteria</Menu.Item>
-                <Menu.Item as='a'>FAQ</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Contact
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
-          </Segment>
-        </Visibility>
-
+            <Container>
+              <Menu.Item><Icon size='large' name='building outline'/></Menu.Item>
+              <Menu.Item as='a' active>Investing With Us</Menu.Item>
+              <Menu.Item as='a'>Properties</Menu.Item>
+              <Menu.Item as='a'>Investment Criteria</Menu.Item>
+              <Menu.Item as='a'>FAQ</Menu.Item>
+              <Menu.Item position='right'>
+                <Button as='a' style={{ marginLeft: '0.5em' }}>
+                  Contact
+                </Button>
+              </Menu.Item>
+            </Container>
+          </Menu>
+          <HomepageHeading />
+        </Segment>
         {children}
       </Responsive>
     )
