24f109ae627d2cf9e0c7ed405425a7ae51a5fefb
ian buchholz <ianbuchholz@gmail.com>
2018-11-29 04:44:00 -0800

    add down arrow scrollspy in InvestingContainer

--- src/components/InvestingContainer.js
+++ src/components/InvestingContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Container, Menu, Segment } from 'semantic-ui-react'
+import { Container, Icon, Menu, Segment } from 'semantic-ui-react'
+import ScrollAnimation from 'react-animate-on-scroll'
+import { Link } from 'react-scroll'
 import Investing from './Investing'
 import Mission from './Mission'
 import Criteria from './Criteria'
 import WhyApartments from './WhyApartments'
 import WhySouthBay from './WhySouthBay'
+
 
 class InvestingContainer extends React.Component {
   constructor(props) {
@@ -36,16 +39,21 @@ class InvestingContainer extends React.Component {
 
     return (
       <Segment className='full'>
-        <Menu pointing vertical style={{marginLeft: '15em', fontSize: '1em'}}>
+        <Menu tabular vertical style={{margin: '10em', fontSize: '1em'}}>
           <Menu.Item active={activeItem === 'investing_with_us'} className='investing_link' id='investing_with_us' onClick={this.handleClick}>INVESTING WITH US</Menu.Item>
           <Menu.Item active={activeItem === 'our_mission'} className='investing_link' id='our_mission' onClick={this.handleClick}>OUR MISSION</Menu.Item>
           <Menu.Item active={activeItem === 'criteria'} className='investing_link' id='criteria' onClick={this.handleClick}>INVESTMENT CRITERIA</Menu.Item>
           <Menu.Item active={activeItem === 'apartments'} className='investing_link' id='apartments' onClick={this.handleClick}>WHY APARTMENTS?</Menu.Item>
           <Menu.Item active={activeItem === 'south_bay'} className='investing_link' id='south_bay' onClick={this.handleClick}>WHY THE SOUTH BAY?</Menu.Item>
         </Menu>
-        <Container>
+        <Container style={{paddingRight: '10em'}}>
           {this.state.view}
         </Container>
+        <ScrollAnimation style={{textAlign: 'center', position: 'absolute', bottom: '0', left: '50%', margin: '2em'}} animateIn='fadeInDown' delay={500} offset={0}>
+          <Link style={{background: 'rgba(255,255,255,0)'}} to='properties' spy={true} smooth={true} duration={500}>
+            <Icon id='down-arrow-investing' size='huge' name='angle down' />
+          </Link>
+        </ScrollAnimation>
       </Segment>
     )
   }
