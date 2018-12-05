8fd782e56cb1dec94ebcd7ed2fa04ab491f6452a
ian buchholz <ianbuchholz@gmail.com>
2018-12-05 02:43:21 -0800

    change layout in WhySouthBay, remove icons and animation

--- src/components/WhySouthBay.js
+++ src/components/WhySouthBay.js
@@ -1,52 +1,23 @@
 import React from 'react'
-import { Container, Grid, Icon } from 'semantic-ui-react'
-import ScrollAnimation from 'react-animate-on-scroll'
+import { Container, Grid } from 'semantic-ui-react'
 
 const WhySouthBay = () => (
   <Container style={{fontSize: '1.3em', padding: '5em'}}>
-    <Grid centered columns={3}>
-      <Grid.Row centered columns={3}>
-        <Grid.Column textAlign='center'>
-          <ScrollAnimation animateIn='flipInY' offset={0}>
-            <Icon name='building' size='massive' circular></Icon>
-          </ScrollAnimation>
-          <ScrollAnimation style={{marginTop: '2em'}} animateIn='slideInUp' offset={0}>
-            <h4>With the local job market and economy and the rise of tech companies relocating to the South Bay, many more young professionals are living and working in the South Bay.</h4>
-          </ScrollAnimation>
+    <Grid columns={2}>
+      <Grid.Row style={{height: '25vh'}}>
+        <Grid.Column>
+          <p>With the local job market and economy and the rise of tech companies relocating to the South Bay, many more young professionals are living and working in the South Bay.</p>
         </Grid.Column>
-        <Grid.Column textAlign='center'>
-          <ScrollAnimation animateIn='flipInY' delay={500} offset={0}>
-            <Icon name='building' size='massive' circular />
-          </ScrollAnimation>
-          <ScrollAnimation style={{marginTop: '2em'}} animateIn='slideInUp' delay={500} offset={0}>
-            <h4>Renting is cheaper than buying, creating an immense demand for rentals.  Los Angeles rents have outpaced inflation over the last decade and the last generation.</h4>
-          </ScrollAnimation>
+        <Grid.Column>
+          <p>Renting is cheaper than buying, creating an immense demand for rentals.  Los Angeles rents have outpaced inflation over the last decade and the last generation.</p>
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row centered columns={3}>
-        <Grid.Column textAlign='center'>
-          <ScrollAnimation animateIn='flipInY' delay={1000} offset={0}>
-            <Icon name='building' size='massive' circular />
-          </ScrollAnimation>
-          <ScrollAnimation style={{marginTop: '2em'}} animateIn='slideInUp' delay={1000} offset={0}>
-            <h4>Since the Great Recession, millennials are opting to rent in lieu of buying. These individuals favor the flexibility of renting over purchasing a property.</h4>
-          </ScrollAnimation>
+      <Grid.Row style={{height: '25vh'}}>
+        <Grid.Column>
+          <p>Since the Great Recession, millennials are opting to rent in lieu of buying. These individuals favor the flexibility of renting over purchasing a property.</p>
         </Grid.Column>
-        <Grid.Column textAlign='center'>
-          <ScrollAnimation animateIn='flipInY' delay={1500} offset={0}>
-            <Icon name='building' size='massive' circular />
-          </ScrollAnimation>
-          <ScrollAnimation style={{marginTop: '2em'}} animateIn='fadeIn' delay={1500} offset={0}>
-            <h4>Los Angeles has lower vacancy rates and steadier appreciation than most other major US cities.</h4>
-          </ScrollAnimation>
-        </Grid.Column>
-        <Grid.Column textAlign='center'>
-          <ScrollAnimation animateIn='flipInY' delay={2000} offset={0}>
-            <Icon name='building' size='massive' circular />
-          </ScrollAnimation>
-          <ScrollAnimation style={{marginTop: '2em'}} animateIn='slideInUp' delay={2000} offset={0}>
-            <h4>People always need a place to live. Even in the event of a market down-turn, there is still income.</h4>
-          </ScrollAnimation>
+        <Grid.Column>
+          <p>Los Angeles has lower vacancy rates and steadier appreciation than most other major US cities.</p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
